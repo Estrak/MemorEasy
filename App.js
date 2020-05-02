2a65f6629404826a4cea96f97d6d3e8a29922bea
Paul Lorgue <paullorgue@gmail.com>
2020-05-02 15:19:29 +0200

    dossier components

--- App.js
+++ App.js
@@ -1,19 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, StatusBar } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Bonjour</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#303030",
+    justifyContent: "center",
   },
+  text: {
+    color: "#fff",
+    textAlign: 'center'
+  }
 });
+
+
+// export default function FirstScreen(){
+//   return (
+//     <View style={styles.container}>
+//       <StatusBar barStyle="light-content" backgroundColor="#303030"/>
+//       <View>
+//         <View style={styles.line}/>
+//         <Text style={{color: '#fff', fontSize: 30, textAlign: 'center'}}>MemorEasy</Text>
+//         <View style={styles.line}/>
+//         <View style={{paddingLeft: 70, paddingRight: 70, marginTop: 70}}>
+//           <Button onPress={begin()} title="Commencer" color="#7EB1BB"/>
+//         </View>
+//       </View>
+//     </View>
+//   );
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#303030',
+//     justifyContent: 'center'
+//   },
+//   title: {
+//     color: '#fff',
+//     fontSize: 30,
+//     textAlign: 'center'
+//   },
+//   line: {
+//     height: 2,
+//     marginLeft: 32,
+//     marginRight: 32,
+//     marginTop: 16,
+//     marginBottom: 16,
+//     backgroundColor: '#fff'
+//   },
+//   button: {
+//     borderRadius: 2,
+//     color: '#8EF1FF',
+//     marginLeft: 30,
+//     marginRight: 30
+//   }
+// });
