eadc22d8e91f23db90cd982284b9c8b922983647
RomainBfn <romain.bonfn@gmail.com>
2020-06-06 18:59:37 +0200

    Custom Button

---
+++ includes/memorEasyFooter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function MemorEasyFooter({navigation}){
+  return (
+    <View>
+      <Text>Footer</Text>
+    </View>
+  );
+}
