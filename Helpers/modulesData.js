b13560073b6b2b521722045782f9482468ab3d7f
RomainBfn <romain.bonfn@gmail.com>
2020-06-06 21:14:54 +0200

    Remise à 0

---
+++ Helpers/modulesData.js
@@ -0,0 +1,51 @@
+export default modulesData = [
+   {
+      id:1,
+      title:"Module 1",
+      poster_path:"",
+      overview:"Lorem ipsum",
+   },
+   {
+      id:2,
+      title:"Module 2",
+      poster_path:"",
+      overview:"Lorem ipsum Lorem ipsum",
+   },
+   {
+      id:3,
+      title:"Module 3",
+      poster_path:"",
+      overview:"Lorem ipsum Lorem ipsum Lorem ipsum",
+   },
+   {
+      id:4,
+      title:"Module 4",
+      poster_path:"",
+      overview:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+   },
+   {
+      id:5,
+      title:"Module 5",
+      poster_path:"",
+      overview:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+   },
+   {
+      id:6,
+      title:"Module 6",
+      poster_path:"",
+      overview:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+   },
+   {
+      id:7,
+      title:"Module 7",
+      poster_path:"",
+      overview:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+   },
+   {
+      id:8,
+      title:"Module 8",
+      poster_path:"",
+      overview:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+   }
+]
+
--- includes/memorEasyHeader.js
+++ includes/memorEasyHeader.js
@@ -1,69 +1,10 @@
 import React from 'react';
-import { View, Image, Text, StyleSheet } from 'react-native';
+import { Text, View } from 'react-native';
 
-export default class MemorEasyHeader extends React.Component{
-
-	render(){
-	return(
-	<View style={styles.Container}>
-    <View style={styles.divTinyLogo}>
-      <Image style={styles.tinyLogo} source={require('../assets/logo.png')}/>
-  	</View>
-    <View style={styles.divTitleText}>
-    <Text style={styles.titleText}>Memor'Easy</Text>
+export default function MemorEasyHeader({navigation}){
+  return (
+    <View>
+      <Text>Header</Text>
     </View>
-  </View>
-		);
-	}
-
+  );
 }
-
-const styles = StyleSheet.create({
-
-  Container:{
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: "#515151",
-    height: 85,
-    elevation: 10,
-    shadowColor: 'black',
-    shadowOffset: { width: 5, height: 5 },
-    shadowOpacity: 0.5,
-    shadowRadius: 10
-  },
-
-  divTinyLogo:{
-    width: 120,
-    height: 120,
-    borderRadius: 120/2,
-    marginLeft: -12,
-    marginTop: 30,
-    backgroundColor: "#515151",
-    justifyContent: 'center',
-    alignItems: 'center',
-    elevation: 10,
-    shadowColor: 'black',
-    shadowOffset: { width: 5, height: 5 },
-    shadowOpacity: 0.5,
-    shadowRadius: 10
-  },
-
-  tinyLogo: {
-    width: 90,
-    height: 90,
-  },
-
-  divTitleText: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginLeft: 30,
-  },
-
-  titleText: {
-    fontSize: 30,
-    fontWeight: "bold",
-    color: "#F4F4F4",
-    letterSpacing: 1,
-    fontFamily: 'Rubik',
-  }
-})
