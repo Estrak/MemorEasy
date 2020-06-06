f9b55a33c59ff3006bf050c10a65193f604b96e1
RomainBfn <romain.bonfn@gmail.com>
2020-06-06 15:53:00 +0200

    HomePage is comming

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import Navigation from './Navigation/Navigation';
+import { StyleSheet, Text, View, Button, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import FirstScreen from './screens/firstScreen.js';
+import Presentation from './screens/presentation.js';
+import HomePage from './screens/homePage.js';
+
+const Stack = createStackNavigator();
 
 export default class App extends React.Component {
-  render() {
+  render(){
     return (
-      <Navigation/>
-    )
+      <NavigationContainer>
+          <Stack.Navigator //Pour cacher la barre supérieure avec le nom des menus
+            screenOptions={{
+              headerShown: false
+            }}
+          >
+          <Stack.Screen
+            name='homePage'
+            component={HomePage}/>
+            <Stack.Screen
+              name='firstScreen'
+              component={FirstScreen}
+              options={{ title: 'Overview'}}/>
+
+            <Stack.Screen
+              name='presentation'
+              component={Presentation}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
   }
 }
--- screens/pageAccueil.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {View , Text, Button, StatusBar, StyleSheet} from 'react-native';
-
-export default function FirstScreen(){
-  return (
-    <Text>Test</Text>
-  );
-}
